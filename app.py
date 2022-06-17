d1d27222c2d3c17c42e906ff2811d6406c79a22d
Mansit  Suman <mansit.suman@themathcompany.com>
2022-06-17 17:17:50 +0530

    MS: Added the data model and created the database

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from email.policy import default
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///prediction.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class visitor_list(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    email_id = db.Column(db.String(100), nullable = False)
+    name = db.Column(db.String(200), nullable = False)
+    company = db.Column(db.String(200), nullable = False)
+    logged_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.name}"
 
 @app.route("/")
 def hello_world():
