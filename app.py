68319224bf7a864be039d4b4f1f61e6b9528c333
Mansit  Suman <mansit.suman@themathcompany.com>
2022-06-17 16:58:46 +0530

    MS: Created a simple app to add users who want to add their names

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+    #return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def products():
+    return "<p>This is the product page</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Flask Demo Project
+
+> **Flask Official Documentation link** - https://flask.palletsprojects.com/en/2.1.x/
+
+> **Bootstrap URL** - https://getbootstrap.com/
+
+Need to learn HTML-intermediate
+
